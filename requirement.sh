4027151289f5d02dc69b4d8cbea5dfce5c9cbd27
100015208-Xia Chen <xiachen0817@163.com>
2025-08-01 18:48:03 +0800

    add requirement

---
+++ requirement.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install v4l-utils
+sudo apt-get install libv4l-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libavutil-dev
+
+# 视频播放解码
+sudo apt install libdvdnav4 libdvd-pkg gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libdvd-pkg
+sudo apt install ubuntu-restricted-extras
